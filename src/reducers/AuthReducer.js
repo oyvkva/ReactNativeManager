eda85dd5d01f8120dd62d26b69c7848ec998ce7d
Oyvind Kvanes <oyvind@kvanes.no>
2017-06-29 18:56:08 +0200

    Add login and error messages to login form

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,16 +1,18 @@
 import { 
   EMAIL_CHANGED, 
-  PASSWORD_CHANGED 
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL 
 } from '../actions/types';
 
 const INITIAL_STATE = { 
   email: '', 
-  password: '' 
+  password: '',
+  user: null,
+  error: '' 
 };
 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action);
-
   switch (action.type) {
     case EMAIL_CHANGED:
       // This first creates a new object.
@@ -19,7 +21,11 @@ export default (state = INITIAL_STATE, action) => {
       // This happens every time we type a new letter into the email field.
       return { ...state, email: action.payload }; 
     case PASSWORD_CHANGED:
-      return { ...state, password: action.payload }; 
+      return { ...state, password: action.payload };
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload, error: '' };
+    case LOGIN_USER_FAIL:
+      return { ...state, error: 'Authentication failed.', password: '' };
     default:
       return state;
   }
